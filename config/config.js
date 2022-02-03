5046b48b7622e23b0ec112109c3b7d8717ed8247
G Sudarshan's macbook air <11sudarshang@gmail.com>
2022-02-03 15:51:47 +0530

    initial commit

---
+++ config/config.js
@@ -0,0 +1,11 @@
+const config={
+    production :{
+        SECRET: process.env.SECRET,
+        DATABASE: process.env.MONGODB_URI
+    }
+}
+
+
+exports.get = function get(env){
+    return config[env]
+}
